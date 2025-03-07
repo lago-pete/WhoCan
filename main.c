7e9d48f37a501fce1d24a153a20698d68c551723
Peter Lagonegro <peter.lagonegro@wsu.edu>
2025-03-06 21:18:26 -0800

    time to actually implement working order

--- main.c
+++ main.c
@@ -1,100 +1,18 @@
-#include <sys/stat.h>
-#include <stdio.h>
-#include <limits.h>
-#include <stdlib.h>
-#include <pwd.h>
-#include <grp.h>
-
-
-#define PATH_MAX        4096 
-
-
-void check_permissions(const char *filename) {
-    struct stat sb;
-
-    if (stat(filename, &sb) == -1) {
-        perror("stat");
-        return;
-    }
-
-    printf("Permissions for %s:\n", filename);
-
-    // Owner permissions
-    printf("Owner: %c%c%c\n",
-           (sb.st_mode & S_IRUSR) ? 'r' : '-',
-           (sb.st_mode & S_IWUSR) ? 'w' : '-',
-           (sb.st_mode & S_IXUSR) ? 'x' : '-');
-
-    // Group permissions
-    printf("Group: %c%c%c\n",
-           (sb.st_mode & S_IRGRP) ? 'r' : '-',
-           (sb.st_mode & S_IWGRP) ? 'w' : '-',
-           (sb.st_mode & S_IXGRP) ? 'x' : '-');
-
-    // Others permissions
-    printf("Others: %c%c%c\n",
-           (sb.st_mode & S_IROTH) ? 'r' : '-',
-           (sb.st_mode & S_IWOTH) ? 'w' : '-',
-           (sb.st_mode & S_IXOTH) ? 'x' : '-');
-}
-
-
-
-int check_path(const char *filename)
-{
-    char resolved_path[PATH_MAX];
-    if (realpath(filename, resolved_path) == NULL) {
-        perror("realpath");
-        return 1;
-    }
-
-    printf("Absolute path: %s\n", resolved_path);
-    return 0;
-}
-
-
-// struct passwd {
-//     char   *pw_name;       /* username */
-//     char   *pw_passwd;     /* user password */
-//     uid_t   pw_uid;        /* user ID */
-//     gid_t   pw_gid;        /* group ID */
-//     char   *pw_gecos;      /* user information */
-//     char   *pw_dir;        /* home directory */
-//     char   *pw_shell;      /* shell program */
-// };
-
-
-int get_info_pw()
-{
-    struct passwd *pw;
-
-    while ((pw = getpwent()) != NULL) {
-        printf("User: %s (UID: %d)   UserDir: %s\n", pw->pw_name, pw->pw_uid, pw->pw_dir);
-    }
-
-    endpwent();
-    return 0;
-}
-
-
-
-int get_info_gr()
-{
-    struct group *gr;
-
-    while ((gr = getgrent()) != NULL) {
-        printf("Group: %s (GID: %d)\n", gr->gr_name, gr->gr_gid);
-    }
-
-    endgrent();
-    return 0;
-}
+#include "whocan.h"
 
 
 
 
 
 int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <file>\n", argv[0]);
+        return 1;
+    }
+    if(check_com(argv[0])) {
+        return 1;
+    }
+
     check_permissions(argv[1]);
     check_path(argv[1]);
     get_info_pw();
---
+++ whocan.h
@@ -0,0 +1,16 @@
+#ifndef WHOCAN_H
+#define WHOCAN_H
+
+#include <sys/stat.h>
+#include <stdio.h>
+#include <limits.h>
+#include <stdlib.h>
+#include <pwd.h>
+#include <grp.h>
+
+#define MAX_PATH 4096
+
+
+
+
+#endif
